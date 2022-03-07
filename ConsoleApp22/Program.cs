8023543f66b4095fdb686e40b5363361fd1fff43
Kukatla Vishalakshmi <Kukatla.Vishalakshmi@ibm.com>
2022-03-07 10:39:14 +0530

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public abstract class Vehicle
+{
+    public abstract void display();
+}
+
+public class car : Vehicle
+{
+    public override void display()
+    {
+        Console.WriteLine("car");
+    }
+}
+
+public class scooty : Vehicle
+{
+    public override void display()
+    {
+        Console.WriteLine("scooty");
+    }
+}
+
+public class Motorcycle : Vehicle
+{
+    public override void display()
+    {
+        Console.WriteLine("Motorcycle");
+    }
+}
+
+public class MyClass
+{
+    public static void Main()
+    {
+        Vehicle v;
+        v = new car();
+        v.display();
+        v = new scooty();
+        v.display();
+        v = new Motorcycle();
+        v.display();
+        Console.ReadLine();
+    }
+}
